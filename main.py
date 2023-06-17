ef629bb45fd067014451b2e68a5f1f61263e138e
SvRomanova <122302319+SvRomanova@users.noreply.github.com>
2023-06-17 11:11:29 +0300

    Update main.py
    v. 2.0

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import time
 
 from pygame import *
 
+
+class Area():
+    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+        self.rect = pygame.Rect(x, y, width, height)  # прямокутник
+        self.fill_color = back
+        if color:
+            self.fill_color = color
+
+    def color(self, new_color):
+        self.fill_color = new_color
+
+    def fill(self):
+        pygame.draw.rect(window, self.fill_color, self.rect)
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+class Picture(Area):
+    def __init__(self, filename, x=0, y=0, width=10, height=10):
+        Area.__init__(self, x=x, y=y, width=width, height=height, color=None)
+        self.image = pygame.image.load(filename)
+
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        
 back = (238, 45, 205)
 
 win_width = 500
